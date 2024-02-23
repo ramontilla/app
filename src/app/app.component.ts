567e69ec1d709a8a00c1579245e33f30cbf333b8
ramontilla <88258660+ramontilla@users.noreply.github.com>
2024-02-23 00:19:38 -0400

    Revert "Configurar wallets (#2)"
    This reverts commit 6dcce03cffcabde08c429ca99f1a58ff6976dc66.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { MatAnchor } from "@angular/material/button";
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
-import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
-import { ShyftApiService } from './services/shyft-api.service';
-import { WalletStore } from '@heavy-duty/wallet-adapter';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { computedAsync } from 'ngxtension/computed-async';
-import { DecimalPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [
-    RouterModule,
-    RouterOutlet,
-    RouterLink,
-    DecimalPipe,
-    MatAnchor,
-    HdWalletMultiButtonComponent,
-  ],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
-  template: `
-    <header class="px-16 pt-20 pb-8 relative">
-      <h1 class="text-center text-5xl mb-4"> Heavy Wallet</h1>
-
-      <div class="flex justify-center mb-4">
-        <hd-wallet-multi-button></hd-wallet-multi-button>
-      </div>
-
-      <nav>
-        <ul class="flex justify-center items-center gap-4">
-          <li>
-            <a [routerLink]="['balance']" mat-raised-button>Balance</a>
-          </li>
-          <li>
-            <a [routerLink]="['settings']" mat-raised-button>Settings</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-  `,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'app';
+}
